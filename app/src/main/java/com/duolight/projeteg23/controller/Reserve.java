1ec0ae84046efd83a608c90c2bee56f5c732fc68
Antoine <antoine.roche@utt.fr>
2022-05-26 10:40:02 +0200

    1er jet, replacement de certains objets, création de la page réserve

---
+++ app/src/main/java/com/duolight/projeteg23/controller/Reserve.java
@@ -0,0 +1,14 @@
+package com.duolight.projeteg23.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reserve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve);
+    }
+}
